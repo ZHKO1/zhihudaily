741751dbb3bb203015b03cf8730174696c82c9ec
ZHKO1 <zhko11993@gmail.com>
2015-05-10 13:03:10 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,18 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  var App = {
+    initialize: function() {
+
+      require([
+        'views/nav'
+      ], function() {
+        Backbone.history.start();
+      });
+
+    }
+  };
+  return App;
+});
